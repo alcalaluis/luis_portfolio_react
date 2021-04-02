1ce39905ad6a48efa6b2830ac9f0d70d5545c86b
alcalaluis <alcalaluis@rocketmail.com>
2021-04-01 20:38:45 -0500

    portfolio page needs work

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -22,28 +22,28 @@ function Contact() {
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
                         <ul className="contact-link">
-                        <a target="_blank" className="contact-links" href="https://www.yahoo.com/">alcalaluis@rocketmail.com</a>
+                        <a rel="noreferrer" target="_blank" className="contact-links" href="https://www.yahoo.com/">alcalaluis@rocketmail.com</a>
                         </ul>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
                         <ul className="contact-link">
-                        <a target="_blank" className="contact-links" href="https://www.linkedin.com/in/luis-alcala-004b45185/">Linkedin</a>
+                        <a rel="noreferrer" target="_blank" className="contact-links" href="https://www.linkedin.com/in/luis-alcala-004b45185/">Linkedin</a>
                         </ul>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
                         <ul className="contact-link">
-                        <a target="_blank" className="contact-links" href="https://github.com/alcalaluis">GitHub</a>
+                        <a rel="noreferrer" target="_blank" className="contact-links" href="https://github.com/alcalaluis">GitHub</a>
                         </ul>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6 offset-md-3">
                         <ul className="contact-link">
-                        <a target="_blank" className="contact-links" href="https://drive.google.com/file/d/1XBGM0zA6VvJ9tMVejD-9N_1vCz1n00L7/view?usp=sharing">Resume</a>
+                        <a rel="noreferrer" target="_blank" className="contact-links" href="https://drive.google.com/file/d/1XBGM0zA6VvJ9tMVejD-9N_1vCz1n00L7/view?usp=sharing">Resume</a>
                         </ul>
                     </div>
                 </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import "./style.css";
+import Corona from "./corona-survival.png";
+import LinkUp from "./link-up.png";
+import Store from "./online-store.png";
+import Index from "./employee-index.png";
+import CoronaSm from "./corona-25.png";
+import IndexSm from "./employee-25.png";
+import StoreSm from "./online-store-25.png";
+import LinkUpSm from "./link-up-25.png";
 
 function Portfolio() {
     return (
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
-                Hello, this is my about page
-        </div>
+        <div>
+            <div className="row">
+                <div className="col-md-6">
+                    <img className="portfolio-sm" src={LinkUpSm} alt="link-up app" />
+                    <img className="portfolio" src={LinkUp} alt="link-up app" /> rel="noreferrer"
+                </div>
+                <div className="col-md-6">
+                    <h4  className="top-app-titles">Link Up Socializing App</h4><br />
+                    <a rel="noreferrer" className="top-app-titles" href="https://github.com/mpapamichalis/link-it">GitHub Link</a><br />
+                    <a rel="noreferrer" className="top-app-titles" href="https://link-up-pro.herokuapp.com/">Deployed App Link</a><br />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <img className="portfolio-sm" src={IndexSm} alt="employee index app" />
+                    <img className="portfolio" src={Index} alt="employee index app" />
+                </div>
+                <div className="col-md-6">
+                    <h4 className="top-app-titles">Employee Directory</h4><br />
+                    <a rel="noreferrer" className="top-app-titles" href="https://github.com/alcalaluis/grand-employee-archives">GitHub Link</a> <br />
+                    <a  rel="noreferrer" className="top-app-titles" href="https://react-app-employee-archives.herokuapp.com/">Deployed App Link</a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <img className="portfolio-sm" src={StoreSm} alt="online store app" />
+                    <img className="portfolio" src={Store} alt="online store app" />
+                </div>
+                <div className="col-md-6">
+                    <h4 className="top-app-titles">Website for Selling Websites</h4><br />
+                    <a rel="noreferrer" className="top-app-titles" href="https://github.com/adamhayward/online_store">GitHub Link</a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <img className="portfolio-sm" src={CoronaSm} alt="corona survival app" />
+                    <img className="portfolio" src={Corona} alt="corona survival app" />
+                </div>
+                <div className="col-md-6">
+                    <h4 className="app-titles">Corona Survival App</h4>
+                    <a rel="noreferrer" className="app-titles" href="https://github.com/mpapamichalis/Corona-Survival">GitHub Link</a><br />
+                    <a rel="noreferrer" className="app-titles" href="https://mpapamichalis.github.io/Corona-Survival/">Deployed App Link</a>
+                </div>
+            </div>
+            
         </div>
 
     );
@@ -15,3 +64,4 @@ function Portfolio() {
 }
 
 export default Portfolio;
+
